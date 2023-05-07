6520eca293c3e9ccc6b45b567117dbe3365a1c8d
Muhammad Shafiq Ahmad Razman <shafiq@Muhammads-MacBook-Air.local>
2023-05-07 22:30:20 +0800

    latest

--- login.php
+++ login.php
@@ -62,10 +62,11 @@
 
     .password-toggle {
       position: absolute;
-      right: 15px;
+      right: 25px;
       top: 50%;
       transform: translateY(-50%);
       cursor: pointer;
+      font-size: 0.9rem;
     }
   </style>
 </head>
@@ -94,11 +95,10 @@
         </div>
         <div class="form-group position-relative">
           <label for="password">Password</label>
-          <input type="password" class="form-control" id="password" name="password" required>
-          <span class="password-toggle" onclick="togglePasswordView()">
-            <i class="fas fa-eye" id="toggle-password-icon"></i>
-
-          </span>
+          <div style="display: flex; align-items: center">
+            <input type="password" class="form-control mr-1" id="password" name="password" required>
+            <i onclick="togglePasswordView()" class="fas fa-eye" id="toggle-password-icon"></i>
+          </div>
         </div>
         <button type="submit" class="btn btn-primary btn-block">Login</button>
       </form>
@@ -109,7 +109,7 @@
   <!-- FontAwesome -->
   <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
   <!-- Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.jsintegrity="
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.jsintegrity=
     sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
     integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
