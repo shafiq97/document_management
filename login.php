e2561b776ae45a5e991144ba6b07884e377c877f
Muhammad Shafiq Ahmad Razman <shafiq@Muhammads-MacBook-Air.local>
2023-05-07 22:21:58 +0800

    latest

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
     body {
       font-family: 'Roboto', sans-serif;
       background-color: #f5f5f5;
-    } 
+    }
 
     .container {
       margin-top: 50px;
@@ -59,13 +59,21 @@
       border-color: #357ae8;
       transform: scale(1.05);
     }
+
+    .password-toggle {
+      position: absolute;
+      right: 15px;
+      top: 50%;
+      transform: translateY(-50%);
+      cursor: pointer;
+    }
   </style>
 </head>
 <body>
   <div class="container">
     <div class="card">
       <?php
-      if(isset($_GET['message'])){
+      if (isset($_GET['message'])) {
         ?>
         <div class="alert alert-success">
           <p>User has been registered successfully</p>
@@ -84,9 +92,13 @@
           <label for="username">Username</label>
           <input type="text" class="form-control" id="username" name="username" required>
         </div>
-        <div class="form-group">
+        <div class="form-group position-relative">
           <label for="password">Password</label>
           <input type="password" class="form-control" id="password" name="password" required>
+          <span class="password-toggle" onclick="togglePasswordView()">
+            <i class="fas fa-eye" id="toggle-password-icon"></i>
+
+          </span>
         </div>
         <button type="submit" class="btn btn-primary btn-block">Login</button>
       </form>
@@ -94,10 +106,11 @@
       <a href="admin/login.php">Login as admin</a>
     </div>
   </div>
+  <!-- FontAwesome -->
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
   <!-- Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.jsintegrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-crossorigin="anonymous"></script>
-
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.jsintegrity="
+    sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
     integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
     crossorigin="anonymous"></script>
@@ -106,9 +119,23 @@ crossorigin="anonymous"></script>
     crossorigin="anonymous"></script>
   <script>
     // Use JavaScript to remove the alert message after a few seconds
-    setTimeout(function() {
+    setTimeout(function () {
       document.querySelector('.alert').remove();
     }, 5000);
+    function togglePasswordView() {
+      const passwordInput = document.getElementById('password');
+      const passwordIcon = document.getElementById('toggle-password-icon');
+
+      if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        passwordIcon.classList.remove('fa-eye');
+        passwordIcon.classList.add('fa-eye-slash');
+      } else {
+        passwordInput.type = 'password';
+        passwordIcon.classList.remove('fa-eye-slash');
+        passwordIcon.classList.add('fa-eye');
+      }
+    }
   </script>
 </body>
 </html>
