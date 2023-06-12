17e3e8d93cb8535e9ad5666f24630c28d9bef92d
Muhammad Shafiq Ahmad Razman <shafiq@Muhammads-MacBook-Air.local>
2023-06-13 00:51:15 +0800

    latest

--- views/profile-user.php
+++ views/profile-user.php
@@ -327,13 +327,6 @@ mysqli_close($conn);
             },
             "type": "file"
           },
-
-          {
-            "data": null,
-            "render": function (data, type, row) {
-              return '<a class="btn btn-primary" href="edit-document.php?id=' + row.doc_id + '">Edit</a> <button class="btn btn-danger delete-btn" data-id="' + row.doc_id + '">Delete</button>';
-            }
-          }
         ]
       });
 
